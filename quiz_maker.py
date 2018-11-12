4559be67b018d65cef4c9aad5f8a77db908d21ae
Kanaan Irwin <kanaanci@yahoo.com>
2018-11-11 22:45:17 -0500

    IdCheck
    Added a check to see if the user id is correct, also added a new functiont to get the students information

--- quiz_maker.py
+++ quiz_maker.py
@@ -1,25 +1,57 @@
 import random
 import datetime
 
-def check_name(fname, lname, student_id):
+def getStudentInfo():
   '''
-  check the student's first name, last name, and ID matches this format:
+  gets the name and ID of the student taking the quiz
+  '''
+  idCheckCounter = 1
+  fName = input("First Name: ").title()
+  lName = input("Last Name: ").title()
+  studentId = input("Student ID (Ex: A12345): ").title()
+  
+  while True:
+    if checkId(studentId):
+      break
+    elif idCheckCounter == 3:
+      print("Too many failed attempts; Exiting program")
+      exit()
+    else:
+      idCheckCounter += 1
+      print("Invalid ID; Please try again\n")
+      studentId = input("Student ID (Ex: A12345): ").title()
+  
+  return fName, lName, studentId
+  
+def checkId(id):
+  '''
+  check the student's ID matches this format:
   a number of 6 digits, starts with letter 'A' then 5 numbers, 
   each number could be any val between 1 AND 9
   After 3 failed attempts, exit
   Valid ID's: A12345, A92154
   Invalid: a12345, 123456, A02456
   '''
-  print("Name: " + fname + " " + lname)
-  print("ID: " + student_id)
+  idLen = len(id[1:])
+    
+  if id[:1] == 'A' and idLen == 5 and id[1:].isdigit():
+    return True
+  else:
+    return False
 
-def read_file():
+def readFile():
   '''
   read test bank, maybe pick 10 random questions
   '''
-  pass
+  testBank = 'TestBank.txt'
+  
+  try:
+    with open(testBank, 'r') as tb:
+      pass
+  except(FileNotFoundError):
+    print(testBank, "was not found")
 
-def output_file():
+def outputFile():
   '''
   student's returned answer bank, name is ID_Fname_Lname
   write in the following data:
@@ -36,7 +68,7 @@ def timer():
   '''
   pass
 
-def check_input():
+def checkInput():
   '''
   Check if student's input matches one of the following:
   true, TRUE, T, t, false, FALSE, f, F
@@ -45,14 +77,8 @@ def check_input():
   pass
 
 def main():
-  fname = input("First Name: ")
-  lname = input("Last Name: ")
-  student_id = input("Student ID (Ex: A12345): ")
-  fname = fname.title()
-  lname = lname.title()
-  student_id = student_id.title()
-
-  check_name(fname, lname, student_id)  
+  readFile()
+  getStudentInfo()
 
 if __name__ == "__main__":
   main()
